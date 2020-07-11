799734092873e5fa0526714001beffde8624ed7e
camhord <camhord@gmail.com>
2020-07-11 03:14:06 -0400

    Himawari 8 image downloading script

---
+++ himawari.py
@@ -0,0 +1,56 @@
+
+import sys
+import requests
+import json
+import shutil
+from datetime import datetime
+from PIL import Image
+from io import BytesIO
+import concurrent.futures
+import threading
+
+BASE_URL = "http://himawari8-dl.nict.go.jp/himawari8/img"
+WIDTH = 550
+VISIBLE_LIGHT = "D531106"
+INFRARED = "INFRARED_FULL"
+DEPTHS = (1, 4, 8, 16, 20)
+
+ZOOM_INDEX = 0
+if len(sys.argv) > 1:
+    ZOOM_INDEX = int(sys.argv[1]) if int(sys.argv[1]) < len(DEPTHS) else 0
+
+print(sys.argv)
+
+def get_himawari_datetime():
+    latest_url = "https://himawari-8.appspot.com/latest"
+    response = requests.get(latest_url)
+    himawari_time = datetime.strptime(json.loads(response.text)["date"], "%Y-%m-%d %H:%M:%S")
+    return himawari_time.strftime("%Y/%m/%d/%H%M%S")
+
+blocks = DEPTHS[ZOOM_INDEX]
+time = get_himawari_datetime()
+url = "/".join((BASE_URL, VISIBLE_LIGHT, "{}d".format(blocks), str(WIDTH), time))
+
+output = Image.new('RGB', (WIDTH * blocks, WIDTH * blocks))
+imglock = threading.Lock()
+
+def download_and_merge(x, y):
+    block_url = url + "_{}_{}.png".format(x, y) 
+    print(block_url)
+    response = requests.get(block_url)
+
+    if not response.ok: 
+        print(response)
+    else:
+        imageblock = response.content
+        with imglock:
+            output.paste(Image.open(BytesIO(imageblock)), (x * WIDTH, y * WIDTH))
+
+xvalues = list(range(blocks)) * blocks
+yvalues = list(range(blocks)) * blocks
+yvalues.sort()
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(download_and_merge, xvalues, yvalues)
+
+output.save("himawari.png")
