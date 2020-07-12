a7e37eb273f03f411a11382563c4fc674d1b126f
Cameron Hord <camhord@gmail.com>
2020-07-12 00:14:17 -0400

    What the fuck is this cv2 shit? Goddamn bastard don't even work.

--- himawari.py
+++ himawari.py
@@ -1,11 +1,12 @@
-
 import sys
 import requests
 import json
 import shutil
 from datetime import datetime
-from PIL import Image
+import cv2
+import numpy
 from io import BytesIO
+import time
 import concurrent.futures
 import threading
 
@@ -19,38 +20,58 @@ ZOOM_INDEX = 0
 if len(sys.argv) > 1:
     ZOOM_INDEX = int(sys.argv[1]) if int(sys.argv[1]) < len(DEPTHS) else 0
 
-print(sys.argv)
+blocks = DEPTHS[ZOOM_INDEX]
 
 def get_himawari_datetime():
     latest_url = "https://himawari-8.appspot.com/latest"
     response = requests.get(latest_url)
     himawari_time = datetime.strptime(json.loads(response.text)["date"], "%Y-%m-%d %H:%M:%S")
-    return himawari_time.strftime("%Y/%m/%d/%H%M%S")
+    return himawari_time
 
-blocks = DEPTHS[ZOOM_INDEX]
-time = get_himawari_datetime()
-url = "/".join((BASE_URL, VISIBLE_LIGHT, "{}d".format(blocks), str(WIDTH), time))
+def get_and_show(query_time):
+    url = "/".join((BASE_URL, VISIBLE_LIGHT, "{}d".format(blocks), str(WIDTH), query_time.strftime("%Y/%m/%d/%H%M%S")))
 
-output = Image.new('RGB', (WIDTH * blocks, WIDTH * blocks))
-imglock = threading.Lock()
+    output = [numpy.zeros((WIDTH, WIDTH, 3), numpy.uint8) * blocks] * blocks
+    imglock = threading.Lock()
 
-def download_and_merge(x, y):
-    block_url = url + "_{}_{}.png".format(x, y) 
-    print(block_url)
-    response = requests.get(block_url)
+    xvalues = list(range(blocks)) * blocks
+    yvalues = list(range(blocks)) * blocks
+    yvalues.sort()
 
-    if not response.ok: 
-        print(response)
-    else:
-        imageblock = response.content
-        with imglock:
-            output.paste(Image.open(BytesIO(imageblock)), (x * WIDTH, y * WIDTH))
+    def download_and_merge(x, y):
+        block_url = url + "_{}_{}.png".format(x, y) 
+        print(block_url)
+        response = requests.get(block_url)
 
-xvalues = list(range(blocks)) * blocks
-yvalues = list(range(blocks)) * blocks
-yvalues.sort()
+        if not response.ok: 
+            print(response)
+        else:
+            imageblock = BytesIO(response.content)
+            with imglock:
+                output[x][y] = cv2.imdecode(numpy.fromstring(imageblock.read(), numpy.uint8), 1)
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(download_and_merge, xvalues, yvalues)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(download_and_merge, xvalues, yvalues)
 
-output.save("himawari.png")
+    return output
+
+oldtime = datetime(1970,1,1,0,0,0,0)
+#while True:
+latest = get_himawari_datetime()
+
+#if latest.timestamp() > oldtime.timestamp():
+    #oldtime = latest
+output = get_and_show(latest)
+if blocks > 1:
+    cvs = map(cv2.hconcat, output)
+    disc = cv2.vconcat(cvs)
+    cv2.imshow("ass", disc)
+else:
+    cv2.imshow("ass", output[0][0])
+    cv2.waitKey(1)
+
+    time.sleep(10)
+        #output.save("himawari.png")
+        #output.show("Himawari 8 | {}".format(latest.strftime("%Y-%m-%d %H:%M:%S")))
+    
+    #time.sleep(300)
